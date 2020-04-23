c93c1233da431ce051e8d79f9a5c868eb63b93e0
zsih1995 <714075406@qq.com>
2020-04-23 22:18:29 +0800

    添加上报数据设备接口

--- app/router/device/index.js
+++ app/router/device/index.js
@@ -1,5 +1,5 @@
 import Router from 'koa-router'
-
+import moment from 'moment'
 let router= Router({
   prefix: 'device'
 })
@@ -22,23 +22,28 @@ router.get('/data/:userId',async (ctx,next)=>{
 // 上报设备数据
 router.post('/data/:userId',async (ctx,next)=>{
   let result = Object.create(null)
-  let userId = ctx.params.userId
+  let userId = Number(ctx.params.userId)
   let resbody = ctx.request.body
   let Data = ctx.app.context.db.data;
-  const nowDate = new Date()
-  const data =await Data.find({
+  let data;
+  const lastData = await Data.find(
+    {
       where:{
         userId:userId,
         deviceId:resbody.deviceId,
-        catalogId:resbody.catalogId
-      }})
-  await Data.create(resbody)
-  // let total = await Data.count()
-  // let list = await Catalog.find()
+        catalogId:resbody.catalogId 
+      },  sort: 'updatedAt desc', limit:1
+    } 
+  )
+  if(moment(lastData[0].updatedAt).format('YYYY-MM-DD') === moment().format('YYYY-MM-DD')){
+    data = await Data.update({id:lastData[0].id},resbody)
+  }else{
+    data = await Data.create({userId:userId,...resbody})
+  }
   result['exception']=false
   result['msg']='请求成功'
-  // result['total']=total
-  // result['list']=list
+  result['data']=data
+
 	ctx.body = result
 });
 
