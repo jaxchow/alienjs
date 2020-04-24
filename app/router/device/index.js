b2fd1d869f31d3576ab275f62db1f0cfd1580136
zsh <714075406@qq.com>
2020-04-24 10:53:52 +0800

    完善设备数据接口

--- app/router/device/index.js
+++ app/router/device/index.js
@@ -9,14 +9,20 @@ router.get('/data/:userId',async (ctx,next)=>{
   let result = Object.create(null)
   let userId = ctx.params.userId
   let param = ctx.query
+  let endDate = param.endDate+' 23:59:59'
   let Data = ctx.app.context.db.data;
-  console.log(ctx.query)
-  // let total = await Data.count()
-  // let list = await Catalog.find()
+  let data = await Data.find().where(
+    {
+      updatedAt:{
+        '>=':param.startDate,
+        '<=':endDate
+      },userId:userId,catalogId:param.type
+    }
+  )
   result['exception']=false
   result['msg']='请求成功'
-  // result['total']=total
-  // result['list']=list
+  result['total']=data.length
+  result['data']=data
 	ctx.body = result
 });
 // 上报设备数据
@@ -35,10 +41,10 @@ router.post('/data/:userId',async (ctx,next)=>{
       },  sort: 'updatedAt desc', limit:1
     } 
   )
-  if(moment(lastData[0].updatedAt).format('YYYY-MM-DD') === moment().format('YYYY-MM-DD')){
-    data = await Data.update({id:lastData[0].id},resbody)
-  }else{
+  if(lastData.length == 0 || moment(lastData[0].updatedAt).format('YYYY-MM-DD') !== moment().format('YYYY-MM-DD')){
     data = await Data.create({userId:userId,...resbody})
+  }else{
+    data = await Data.update({id:lastData[0].id},resbody)
   }
   result['exception']=false
   result['msg']='请求成功'
