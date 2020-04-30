edfb1d680e5c1041d10b4843dc33e955ee824020
zsh <714075406@qq.com>
2020-04-30 18:00:53 +0800

    增加总数字段，更改时间筛选

--- app/router/rank/index.js
+++ app/router/rank/index.js
@@ -10,11 +10,32 @@ router.get('/:userId',async (ctx,next)=>{
   let Catalog = ctx.app.context.db.catalog;
   let userId = Number(ctx.params.userId)
   let param = ctx.query // startDate endDate startNum endNum type
-  let endDate = param.endDate+' 23:59:59'
+  let endDate = param.endDate+'T23:59:59Z'
   // 当前用户的数据
   let myData 
   // 排行的用户数据
   let list = await Data.find(
+    {
+      where:{
+        updatedAt:{
+          '>':param.startDate,
+          '<':endDate
+        },
+        catalogId:param.catalogId
+      },
+      groupBy:['userId'],
+      sum:['calorie','value']
+    }
+  )
+  .sort('value desc','calorie desc').skip(param.startNum).limit(param.endNum-param.startNum+1)
+  for(let i = 0;i<list.length;i++){
+    if(list[i].userId === userId){
+      myData = list[i]
+      break;
+    }
+  }
+  let catalogData = await Catalog.findOne(param.catalogId)
+  let total = await Data.count(
     {
       where:{
         updatedAt:{
@@ -24,20 +45,11 @@ router.get('/:userId',async (ctx,next)=>{
         catalogId:param.catalogId
       },
       groupBy:['userId'],
-      sum:['calorie','value']
     }
-  ).sort('value desc','calorie desc').skip(param.startNum).limit(param.endNum-param.startNum+1)
-
-  for(let i = 0;i<list.length;i++){
-    if(list[i].userId === userId){
-      myData = list[i]
-      break;
-    }
-  }
-  let catalogData = await Catalog.findOne(param.type)
-
+  )
 	ctx.body = {
     myData:myData,
+    total:total,
     list:list,
     catalogId:catalogData.id,
     type:catalogData.type,
@@ -50,7 +62,7 @@ router.get('/',async (ctx,next)=>{
   let Data = ctx.app.context.db.data;
   let Catalog = ctx.app.context.db.catalog;
   let param = ctx.query // startDate endDate startNum endNum type
-  let endDate = param.endDate+' 23:59:59'
+  let endDate = param.endDate+'T23:59:59Z'
  
   // 排行的用户数据
   let list = await Data.find(
@@ -66,10 +78,21 @@ router.get('/',async (ctx,next)=>{
       sum:['calorie','value']
     }
   ).sort('value desc','calorie desc').skip(param.startNum).limit(param.endNum-param.startNum+1)
-  let catalogData = await Catalog.findOne(param.type)
-  
-  console.log(catalogData)
+  let catalogData = await Catalog.findOne(param.catalogId)
+  let total = await Data.count(
+    {
+      where:{
+        updatedAt:{
+          '>=':param.startDate,
+          '<=':endDate
+        },
+        catalogId:param.catalogId
+      },
+      groupBy:['userId'],
+    }
+  )
 	ctx.body = {
+    total:total,
     list:list,
     catalogId:catalogData.id,
     type:catalogData.type,
