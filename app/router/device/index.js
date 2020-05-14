a0e20a3a0f68276b77f9b3b150d2158713749e00
zsh <714075406@qq.com>
2020-05-14 14:19:02 +0800

    将token校验改到接口上

--- app/router/device/index.js
+++ app/router/device/index.js
@@ -6,96 +6,115 @@ let router= Router({
 
 // 设备数据
 router.get('/data/:userId',async (ctx,next)=>{
-  let userId = ctx.params.userId
-  let param = ctx.query
-  let endDate = param.endDate+'T23:59:59Z'
-  let Data = ctx.app.context.db.data;
-  let Catalog = ctx.app.context.db.catalog;
-  let Device = ctx.app.context.db.device;
-  let data = await Data.find(
-    {
-      where:{
-        updatedAt:{
-          '>=':param.startDate,
-          '<=':endDate
+  let User =ctx.app.context.db.user
+  let token = ctx.request.header['token']
+  let tokenData = await User.find({unionId:token})
+  if(tokenData.length>0){
+    let userId = ctx.params.userId
+    let param = ctx.query
+    let endDate = param.endDate+'T23:59:59Z'
+    let Data = ctx.app.context.db.data;
+    let Catalog = ctx.app.context.db.catalog;
+    let Device = ctx.app.context.db.device;
+    let data = await Data.find(
+      {
+        where:{
+          updatedAt:{
+            '>=':param.startDate,
+            '<=':endDate
+          },
+          userId:userId,
+          catalogId:param.catalogId
         },
-        userId:userId,
-        catalogId:param.catalogId
-      },
-      groupBy:['userId','catalogId'],
-      sum:['time','fatcut','calorie','value']
+        groupBy:['userId','catalogId'],
+        sum:['time','fatcut','calorie','value']
+      }
+    )
+    if(data[0]){
+      let catalogData = await Catalog.findOne(param.catalogId)
+      let deviceData = await Device.findOne({catalogId:param.catalogId,userId:userId})
+      data[0].unit = catalogData.unit
+      data[0].name = catalogData.name
+      data[0].type = catalogData.type
+      if(deviceData){
+        data[0].index = deviceData.index
     }
-  )
-  if(data[0]){
-    let catalogData = await Catalog.findOne(param.catalogId)
-    let deviceData = await Device.findOne({catalogId:param.catalogId,userId:userId})
-    data[0].unit = catalogData.unit
-    data[0].name = catalogData.name
-    data[0].type = catalogData.type
-    if(deviceData){
-	    data[0].index = deviceData.index
-	}
-    ctx.body = data[0]
+      ctx.body = data[0]
+    }else{
+      let catalogData = await Catalog.findOne(param.catalogId)
+      let deviceData = await Device.findOne({catalogId:param.catalogId,userId:userId})
+      if(deviceData){
+        ctx.body = {
+          userId:userId,
+          catalogId:param.catalogId,
+          time:0,
+          fatcat:0,
+          calorie:0,
+          value:0,
+          unit:catalogData.unit,
+          name:catalogData.name,
+          type:catalogData.type,
+          index:deviceData.index
+        }
+      }else{
+        ctx.body={}
+      }
+    }
   }else{
-    let catalogData = await Catalog.findOne(param.catalogId)
-    let deviceData = await Device.findOne({catalogId:param.catalogId,userId:userId})
-    if(deviceData){
-    ctx.body = {
-      userId:userId,
-      catalogId:param.catalogId,
-      time:0,
-      fatcat:0,
-      calorie:0,
-      value:0,
-      unit:catalogData.unit,
-      name:catalogData.name,
-      type:catalogData.type,
-      index:deviceData.index
-    }
-	}else{
-	 ctx.body={}
-	}
-  }
-  
+    ctx.status=401
+  } 
 });
 // 上报设备数据
 router.post('/data/:userId',async (ctx,next)=>{
-  let userId = Number(ctx.params.userId)
-  let resbody = ctx.request.body
-  let Data = ctx.app.context.db.data;
-  let data;
-  const lastData = await Data.find(
-    {
-      where:{
-        userId:userId,
-        deviceId:resbody.deviceId,
-        catalogId:resbody.catalogId 
-      },  sort: 'updatedAt desc', limit:1
-    } 
-  )
-  if(lastData.length == 0 || moment(lastData[0].updatedAt).format('YYYY-MM-DD') !== moment().format('YYYY-MM-DD')){
-    data = await Data.create({userId:userId,...resbody})
+  let User =ctx.app.context.db.user
+  let token = ctx.request.header['token']
+  let tokenData = await User.find({unionId:token})
+  if(tokenData.length>0){
+    let userId = Number(ctx.params.userId)
+    let resbody = ctx.request.body
+    let Data = ctx.app.context.db.data;
+    let data;
+    const lastData = await Data.find(
+      {
+        where:{
+          userId:userId,
+          deviceId:resbody.deviceId,
+          catalogId:resbody.catalogId 
+        },  sort: 'updatedAt desc', limit:1
+      } 
+    )
+    if(lastData.length == 0 || moment(lastData[0].updatedAt).format('YYYY-MM-DD') !== moment().format('YYYY-MM-DD')){
+      data = await Data.create({userId:userId,...resbody})
+    }else{
+      data = await Data.update({id:lastData[0].id},resbody)
+    }
+    ctx.body = data
   }else{
-    data = await Data.update({id:lastData[0].id},resbody)
+    ctx.status=401
   }
-
-	ctx.body = data
 });
 
 // 修改我的目标
 router.put('/data/index',async (ctx,next)=>{
-  let resbody = ctx.request.body
-  let Device = ctx.app.context.db.device
-  let data = await Device.update(
-    {
-        deviceId:resbody.deviceId,
-    },
-    {index:resbody.index}
-  )
-  if(data[0]){
-    ctx.body = data[0]
+  let User =ctx.app.context.db.user
+  let token = ctx.request.header['token']
+  let tokenData = await User.find({unionId:token})
+  if(tokenData.length>0){
+    let resbody = ctx.request.body
+    let Device = ctx.app.context.db.device
+    let data = await Device.update(
+      {
+          deviceId:resbody.deviceId,
+      },
+      {index:resbody.index}
+    )
+    if(data[0]){
+      ctx.body = data[0]
+    }else{
+      ctx.body = {message:"修改失败"}
+    }
   }else{
-    ctx.body = {message:"修改失败"}
+    ctx.status=401
   }
 })
 
--- app/router/user/index.js
+++ app/router/user/index.js
@@ -13,65 +13,97 @@ router.get('/plantinfo',async (ctx,next)=>{
 // 用户信息
 router.get('/:id',async (ctx,next)=>{
   let User =ctx.app.context.db.user
-  let data =await User.findOne(ctx.params.id)
-  if(data){
-	  ctx.body=data
+  let token = ctx.request.header['token']
+  let tokenData = await User.find({unionId:token})
+  if(tokenData.length>0){
+    let data =await User.findOne(ctx.params.id)
+    if(data){
+      ctx.body=data
+    }else{
+      ctx.body={}
+    }
   }else{
-	  ctx.body={}
+    ctx.status = 401
   }
 })
 
 // 用户设备
 
 router.get('/:userId/device',async (ctx,next)=>{
-  let Device = ctx.app.context.db.device;
-  let Catalog = ctx.app.context.db.catalog;
-  //这里应该将选出来的数据与设备类型表之间做关联的
-  let deviceData = await Device.find({userId:ctx.params.userId})
-  for(let i=0;i<deviceData.length;i++){
-    let catalogData = await Catalog.findOne(deviceData[i].catalogId)
-    deviceData[i].catalog = catalogData
+  let User =ctx.app.context.db.user
+  let token = ctx.request.header['token']
+  let tokenData = await User.find({unionId:token})
+  if(tokenData.length>0){
+    let Device = ctx.app.context.db.device;
+    let Catalog = ctx.app.context.db.catalog;
+    let deviceData = await Device.find({userId:ctx.params.userId})
+    for(let i=0;i<deviceData.length;i++){
+      let catalogData = await Catalog.findOne(deviceData[i].catalogId)
+      deviceData[i].catalog = catalogData
+    }
+    ctx.body=deviceData
+  }else{
+    ctx.status=401
   }
-
-	ctx.body=deviceData
+  
 });
 
 // 用户信息修改
 router.put('/:id',async (ctx,next)=>{
-  let User = ctx.app.context.db.user
-  let reqbody = ctx.request.body
-  let data = await User.update(ctx.params,reqbody)
-  if(data[0]){
-    ctx.body = data[0]
+  let User =ctx.app.context.db.user
+  let token = ctx.request.header['token']
+  let tokenData = await User.find({unionId:token})
+  if(tokenData.length>0){
+    let reqbody = ctx.request.body
+    let data = await User.update(ctx.params,reqbody)
+    if(data[0]){
+      ctx.body = data[0]
+    }else{
+      ctx.body = {}
+    }
   }else{
-    ctx.body = {}
+    ctx.status=401
   }
 })
 
 // 用户附加
 router.get('/:id/plant',async (ctx,next)=>{
-  let Plant =ctx.app.context.db.plant
-  let data =await Plant.findOne(ctx.params.id)
-  if(data){
-    ctx.body=data
+  let User =ctx.app.context.db.user
+  let token = ctx.request.header['token']
+  let tokenData = await User.find({unionId:token})
+  if(tokenData.length>0){
+    let Plant =ctx.app.context.db.plant
+    let data =await Plant.findOne(ctx.params.id)
+    if(data){
+      ctx.body=data
+    }else{
+      ctx.body={}
+    }
   }else{
-    ctx.body={}
+    ctx.status=401
   }
 })
 
 
 // 用户附加修改
 router.put('/:id/plant',async (ctx,next)=>{
-  let Plant =ctx.app.context.db.plant
-  let id = ctx.params.id
-  let reqbody = ctx.request.body
-  let data = await Plant.findOne(id)
-  if(data){
-    let dataArr = await Plant.update({userId:id},reqbody)
-    ctx.body = dataArr[0]
+  let User =ctx.app.context.db.user
+  let token = ctx.request.header['token']
+  let tokenData = await User.find({unionId:token})
+  if(tokenData.length>0){
+    let Plant =ctx.app.context.db.plant
+    let id = ctx.params.id
+    let reqbody = ctx.request.body
+    let data = await Plant.findOne(id)
+    if(data){
+      let dataArr = await Plant.update({userId:id},reqbody)
+      ctx.body = dataArr[0]
+    }else{
+      let data = await Plant.create({userId:id,...reqbody})
+      ctx.body = data
+    }
   }else{
-    let data = await Plant.create({userId:id,...reqbody})
-    ctx.body = data
+    ctx.status=401
   }
 })
 
