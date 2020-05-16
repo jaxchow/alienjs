4fe3170de3753655708cf01a77f7125b2929f1f0
jaxchow <you@example.com>
2020-05-16 15:29:41 +0800

    feat: remove connect index =0

--- app/router/device/index.js
+++ app/router/device/index.js
@@ -126,7 +126,7 @@ router.post('/connect',async (ctx,next)=>{
 	let device=await Device.findOne({deviceId:resbody.deviceId})
 	console.log("length",device)
 	if(!device){
-	 resbody.index=10
+	 //resbody.index=10
 	 data = await Device.create(resbody)
 	}
   if(data){
